8f4eb1a91a42c1dc23117996d8fe6d7860e61501
jdiazcons <jdiazc@gmail.com>
2024-07-07 13:20:59 -0400

    Versión final juego número secreto
    Versión final juego número secreto , curso Oracle One /Alura 7 de julio 13.20

---
+++ app3.js
@@ -0,0 +1,9 @@
+let nota1 = 7;
+let nota2 = 6;
+let nota3 = 3;
+let nota4 = 5;
+
+function calcularPromedio(nota1, nota2, nota3, nota4){
+    let promedio = (nota1 +  nota2 +  nota3 +  nota4)/4
+    return promedio;
+}
